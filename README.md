9cf66bca44c989e5860c666f972b5ed723a739d9
thehappydinoa <thehappydinoa@gmail.com>
2017-07-22 19:49:53 -0400

    Initial Commit pt 2

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# AQUOS-Remote-Python
+# Sharp AQUOS Remote Python
+
+Python module for sending Remote Control Codes to your Sharp AQUOS Smart TV
+
+## IP Setup
+
+1. Go to MENU
+2. Go to the Inital Setup Tab
+3. Go to Network Setup
+4. Manual Setup (Click Yes)
+5. Go down to IP Setup
+
+## Remote Setup
+
+1. Go to MENU
+2. Go to the Inital Setup Tab
+3. Go to AQUOS Remote Control
+4. Enable AQUOS Remote 
+
+## Usage
+
+** Import the module
+
+'''
+	import aqous
+'''
+
+** Set IP
+
+'''
+	aquos.set_ip('xxx.xxx.xxx.xxx')
+'''
+
+** Set Login Info (If applicable)
+
+'''
+	aquos.set_login('username','password')
+'''
+
+** TV Commands
+
+'''
+	aquos.tv_on() # Will only work if you last turned off your tv with the aquos.tv_off() function 
+	
+	aquos.tv_off() # Puts TV into standby mode
+	
+	aquos.set_tv_input(x) # Sets TV input to input x
+	
+	aquos.set_tv_volume(xx) # Sets TV volume 0-100
+	
+	aquos.mute_toggle() # Toggles mute
+	
+	aquos.mute_on() # Turns mute on
+	
+	aquos.mute_off() # Turns mute off
+'''
+
+### Customize AQOUS.py
+
+When using aquos you can either manualully adjust the values in aquos.py or you can set them using the functions 
+
+## DEPENDENCIES
+
+This has been tested with Python 2.6 and 2.7.
+
+## LICENSE
+
+MIT License
---
+++ aquos.py
@@ -0,0 +1,88 @@
+''' 
+|  @file aquos.py
+|  @brief A Small module for interactions with any Sharp AQOUS TVs
+|  
+'''
+
+import socket, logging, time
+
+logging.basicConfig(level=logging.DEBUG)
+
+class aquos():
+	
+	tv_ip = ''
+	login = False
+	username = ''
+	password = ''
+	
+	def send_command(command):
+		if (tv_ip == '')
+			logging.critical("Critical exception: No IP set")
+			break
+		else:
+			try:
+				s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+				s.connect((tv_ip, 10002))
+				if (login):
+					s.send(username +"\r" + password +"\r")
+					logging.debug(s.recv(1024))
+				s.send(command)
+				logging.debug(s.recv(1024))
+				return True
+			except Exception, e:
+				logging.critical("Critical exception: " + str(e))
+				break
+		
+	def tv_off():
+		send_command("RSPW2   \r\rPOWR0   \r")
+		return True
+
+	def tv_on():
+		send_command("POWR1   \r")
+		return True
+
+	def mute_toggle():
+		send_command("MUTE0   \r")
+		return True
+	
+	def mute_on():
+		send_command("MUTE1   \r")
+		return True
+	
+	def mute_off():
+		send_command("MUTE2   \r")
+		return True
+	
+	def set_tv_volume(level):
+		if (level <= 100 and level >= 0)
+			send_command("VOLM" + level + "   \r")
+		return True
+
+	def set_tv_input(input):
+		send_command("IAVD" + input + "   \r")
+		return True
+		
+	def set_login(user,passwd)
+		if not login:
+			login = True
+			username = str(user)
+			password = str(passwd)
+		return True
+		
+	def set_ip(ip)
+		tv_ip = str(ip)
+		return True
+		
+		
+if __name__ == "__main__":		
+	
+	# Example
+	aquos.set_ip('192.168.1.12')
+	'''
+	Enter username and password if it applies to you
+	aquos.set_login(username,password)
+	'''
+	aquos.tv_on()
+	aquos.set_tv_volume(30)
+	
+	
