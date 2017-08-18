b7ebef8ffe098c38bd92fe8538762720017caad2
thehappydinoa <thehappydinoa@gmail.com>
2017-08-18 07:46:06 -0400

    Major Update

--- aquos.py
+++ aquos.py
@@ -30,13 +30,37 @@ def send_command(command):
 		except Exception, e:
 			return "error"
 	
-def tv_off():
-	return send_command("RSPW2   \r\rPOWR0   \r")
+def off():
+	return send_command("POWR0   \r")
+	
+def set_standbymode():
+	return send_command("RSPW2   \r\r")
 
-def tv_on():
+def on():
 	return send_command("POWR1   \r")
+	
+def play():
+	return remote_number(16)
+	
+def pause():
+	return remote_number(16)
+	
+def stop():
+	return remote_number(20)
+	
+def rewind():
+	return remote_number(15)
+	
+def fast_forward():
+	return remote_number(17)
+	
+def skip_forward():
+	return remote_number(21)
+	
+def skip_back():
+	return remote_number(19)
 
-def mute_toggle():
+def toggle_mute():
 	return send_command("MUTE0   \r")
 
 def mute_on():
@@ -44,14 +68,51 @@ def mute_on():
 
 def mute_off():
 	return send_command("MUTE2   \r")
+	
+def volume_up():
+	return remote_number(33)
+	
+def volume_down():
+	return remote_number(32)
 
 def set_tv_volume(level):
 	if (level <= 100 and level >= 0):
 		return send_command("VOLM" + level + "   \r")
 	return "error"
-
+	
+def up():
+	return remote_number(41)
+	
+def right():
+	return remote_number(44)
+	
+def down():
+	return remote_number(42)
+	
+def left():
+	return remote_number(43)
+	
+def favorite_app(number):
+	if number == 1:
+		return remote_number(55)
+	elif number == 2:
+		return remote_number(56)
+	elif number == 2:
+		return remote_number(57)
+		
+def netflix():
+	return remote_number(59)
+		
 def set_tv_input(input):
 	return send_command("IAVD" + input + "   \r")
+	
+def toggle_input():
+	return send_command("ITGD   \r")
+	
+def remote_number(number):
+	return send_command("RCKY" + number + "   \r"
+	
+def 
 	
 def set_login(user,passwd):
 	global username, password
