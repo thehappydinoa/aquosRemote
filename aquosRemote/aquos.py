7068157e759e6bc5529aca73eac5b63cc60a9713
Aidan Holland <thehappydinoa@gmail.com>
2018-06-03 15:13:08 -0400

    Better input handling

--- aquosRemote/aquos.py
+++ aquosRemote/aquos.py
@@ -1,4 +1,5 @@
 import socket
+from re import sub
 from time import sleep
 from contextlib import closing
 
@@ -155,8 +156,9 @@ class aquosTV(object):
     def netflix(self):
         return self.remote_number(59)
 
-    def set_input(self, input_name):
-        return self.send_command("IAVD" + str(input_name))
+    def set_input(self, input_number):
+        input_number = sub("\D", "", input_number.strip()
+        return self.send_command("IAVD" + str(input_number))
 
     def toggle_input(self):
         return self.send_command("ITGD1")
@@ -180,7 +182,7 @@ class aquosTV(object):
 
 if __name__ == "__main__":
     # Example/Test
-    aquos = aquosTV("192.168.1.2", setup=True)
+    aquos=aquosTV("192.168.1.2", setup=True)
     aquos.on()
     sleep(1)
     # print(aquos.get_info())
